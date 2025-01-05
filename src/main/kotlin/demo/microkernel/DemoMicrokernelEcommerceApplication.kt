f9804ef0771f97884661b1190ce8cae60279b281
Luca <luca_niklas.franke@stud.htwk-leipzig.de>
2025-01-05 18:35:30 +0100

    initial commit

---
+++ src/main/kotlin/demo/microkernel/DemoMicrokernelEcommerceApplication.kt
@@ -0,0 +1,11 @@
+package demo.microkernel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoMicrokernelEcommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoMicrokernelEcommerceApplication>(*args)
+}
---
+++ src/test/kotlin/demo/microkernel/DemoMicrokernelEcommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package demo.microkernel
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoMicrokernelEcommerceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
